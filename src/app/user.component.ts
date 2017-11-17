c8b6180732a6f10fc2db01cf6a15314aa46a4c4c
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-17 11:28:46 -0500

    EventEmitter with @Output decorator

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" [(ngModel)]="name">
+    <input type="text" (input)="onUserInput($event)" [value]="name">
+    <!-- <input type="text" [(ngModel)]="name"> -->
     <p>Hello {{ name }}!</p>
     <p>I'm the user component</p>
 
@@ -11,8 +12,10 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
   @Input() name = 'Art';
+  @Output() nameChanged = new EventEmitter<string>();
 
   onUserInput(event) {
-    this.name = event.target.value;
+    // this.name = event.target.value;
+    this.nameChanged.emit(event.target.value);
   }
 }
