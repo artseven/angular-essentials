a998600ee12c1c348be9fec22a509527daab4614
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-21 16:36:59 -0500

    types for lodash installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import 'lodash';
 
-declare var _: any;
+// declare var _: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
